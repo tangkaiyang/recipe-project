c84bd227f8e7d820fcc3478e0afa81bc3642c4f0
大胖子 <945541696@qq.com>
2022-05-18 07:52:17 +0800

    ApplicationListener<ContextRefreshedEvent> and bootstrap

--- src/main/java/guru/springframework/recipeproject/domain/Category.java
+++ src/main/java/guru/springframework/recipeproject/domain/Category.java
@@ -3,6 +3,7 @@ package guru.springframework.recipeproject.domain;
 import sun.security.provider.PolicyParser;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -16,7 +17,7 @@ public class Category {
 
     private String description;
     @ManyToMany(mappedBy = "categories")
-    private Set<Recipe> recipes;
+    private Set<Recipe> recipes = new HashSet<>();
 
     public Long getId() {
         return id;
--- src/main/java/guru/springframework/recipeproject/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeproject/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.recipeproject.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -17,6 +18,7 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+    @Lob
     private String directions;
 
     // Large object
@@ -31,10 +33,10 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     public Long getId() {
         return id;
