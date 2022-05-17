fdae6edc9bd15d89e1e2d7f11482e7b89e1b839c
大胖子 <945541696@qq.com>
2022-05-17 22:14:41 +0800

    many to many

--- src/main/java/guru/springframework/recipeproject/domain/Recipe.java
+++ src/main/java/guru/springframework/recipeproject/domain/Recipe.java
@@ -27,6 +27,12 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
@@ -124,5 +130,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
