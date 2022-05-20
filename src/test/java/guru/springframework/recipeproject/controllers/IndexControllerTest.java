4c552354742baa7502a8ff660a87c37c26644aa7
大胖子 <945541696@qq.com>
2022-05-20 22:45:01 +0800

    MockMvc

--- src/test/java/guru/springframework/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/recipeproject/controllers/IndexControllerTest.java
@@ -8,13 +8,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 /**
@@ -32,6 +39,14 @@ class IndexControllerTest {
     @BeforeEach
     void setUp() {
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+
     }
 
     @Test
